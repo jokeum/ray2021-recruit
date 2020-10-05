063a93b5b4f2b80aea373fbdb7013b6b1344efab
Erickson Zhuang <bingluen0224@gmail.com>
2020-10-05 18:06:06 +0800

    Update social meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,14 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
+      { hid: 'og:title', property: 'og:title', content: 'Rescue Action by Youth .gov 4.0' },
+      { hid: 'og:description', property: 'og:description', content: '一場改造公部門網站的熱血行動！' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://ray.pdis.nat.gov.tw' },
+      { hid: 'og:image', property: 'og:image', content: 'https://ray.pdis.nat.gov.tw/repo-social-preivew-crop.jpg' },
+      { hid: 'og:locale', property: 'og:locale', content: 'zh-TW' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Rescue Action by Youth .gov' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -47,9 +54,9 @@ export default {
         title: 'Rescue Action by Youth .gov 4.0',
         site_name: 'Rescue Action by Youth .gov',
         description: '一場改造公部門網站的熱血行動！',
-        img: '/repo-social-preivew-crop.jpg',
+        img: 'https://ray.pdis.nat.gov.tw/repo-social-preivew-crop.jpg',
         locale: 'zh-TW',
-        twitter_card: '一場改造公部門網站的熱血行動！',
+        twitter_card: 'https://ray.pdis.nat.gov.tw/repo-social-preivew-crop.jpg',
         themeColor: '#ee6047'
       }
     ]
